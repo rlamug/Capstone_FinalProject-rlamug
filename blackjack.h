ad50fd8025e14206170f5e7d218b532255156866
rlamug <rlamug@mail.csuchico.edu>
2017-12-08 22:39:18 -0800

    Blackjack.h

--- blackjack.h
+++ blackjack.h
@@ -14,10 +14,9 @@ class blackjack {
 
 
 private:
-
+// int deckOfCards [52] = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}
 
 public:
-
 void mainMenu();
 int randomCard();
 void showHands(int, int, int, int);
